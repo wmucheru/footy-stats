1c043c7c406b307a857dcc2dd758cdebafc9aa95
William Mucheru <willyk99@gmail.com>
2017-12-02 11:44:53 +0300

    Add api logic, update readme

---
+++ src/pages/Landing.jsx
@@ -0,0 +1,17 @@
+import React, { Component } from 'react'
+import { NavLink } from 'react-router-dom'
+
+export default class Landing extends Component{
+
+    constructor(props){
+        super(props)
+        
+        this.state = {
+            competitions: []
+        }
+    }
+
+    render(){
+
+    }
+}
---
+++ src/utils/api.js
@@ -0,0 +1,32 @@
+import axios from 'axios'
+
+const baseURL = 'http://api.football-data.org/v1/'
+const URL = {
+    competitions: baseURL + 'competitions',
+    teams: baseURL + 'teams',
+    fixtures: baseURL + 'fixtures'
+}
+
+const makeAPIRequest = (endpoint, params) => {
+    const options = {
+        headers: { 'X-Auth-Token': '6740a08a53f34b9b9df5c0c8ffd944fc' },
+        params
+    }
+
+    return axios.get(endpoint, options)
+}
+
+export default FootyAPI = {
+    
+    getCompetitions: function(params){
+        return makeAPIRequest(URL.competitions, params)
+    },
+
+    getTeams: function(params){
+        return makeAPIRequest(URL.teams, params)
+    },
+
+    getFixtures: function(params){
+        return makeAPIRequest(URL.fixtures, params)
+    }
+}
